7c19fb4af817908f37a82f6f1eb95e9692932252
Ricky_home_pc <guagin0972@gmail.com>
2020-04-04 11:41:37 +0800

    add meal prepare.

---
+++ src/order/domain/meal/service/prepare-meal-service.ts
@@ -0,0 +1,17 @@
+import { MealEventPublisher } from "../meal-event-publisher";
+import { MealRepository } from "../meal-repository";
+import { MealId } from "../meal";
+
+export class PrepareMealService{
+    constructor(private mealRepository: MealRepository, private mealEventPublisher: MealEventPublisher){}
+
+    async prepare(id: MealId): Promise<void>{
+        const meal = await this.mealRepository.ofId(id)
+        
+        meal.prepare()
+
+        await this.mealRepository.save(meal)
+
+        this.mealEventPublisher.mealPrepared(meal, 'uber')        
+    }
+}
